049821c173ba917922cd3018ab130ef0d2121da0
Joe Begley <joebegley15@gmail.com>
2018-06-29 14:48:08 -0400

    Created hero service. Had hero details flow from hero detail component rather than from the hero component itself

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-
 import { HEROES } from '../mock-heroes';
- 
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -12,21 +11,14 @@ export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
 
-  selectedHero : Hero;
+  selectedHero: Hero;
 
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
- 
   constructor() { }
- 
+
   ngOnInit() {
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
- 
 }
