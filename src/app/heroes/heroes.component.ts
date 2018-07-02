0a89e10b3b74f6282e3f279c91f02f09a85242c2
Joe Begley <joebegley15@gmail.com>
2018-07-02 16:05:24 -0400

    Completed routes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,10 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
@@ -21,10 +18,6 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    .subscribe(heroes => this.heroes = heroes);
   }
 }
